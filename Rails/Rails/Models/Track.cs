e56a8ad71d8891b1f288c8ec5da1df0d9487b664
Nivade <nhavandeursen@gmail.com>
2016-06-02 18:40:08 +0200

    Add [Key] attribute to Id

--- Rails/Rails/Models/Track.cs
+++ Rails/Rails/Models/Track.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,7 @@ namespace Rails.Models
 {
     public class Track
     {
-
+        [Key]
         public int Id { get; set; }
         public int Number { get; set; }
         public int Length { get; set; }
