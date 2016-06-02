82a3ec264ae2f95124a836be006260481bf19233
Nivade <nhavandeursen@gmail.com>
2016-06-02 18:25:25 +0200

    Add [Key] attribute

--- Rails/Rails/Models/Permission.cs
+++ Rails/Rails/Models/Permission.cs
@@ -10,10 +10,8 @@ namespace Rails.Models
 {
     public class Permission
     {
-        
+        [Key]
         public int Id { get; set; }
-
-        
         public string Description { get; set; }
     }
 
