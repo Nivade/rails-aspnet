aca265b758c42b5c6b917c89b9fbd5793834b26d
Nivade <nhavandeursen@gmail.com>
2016-06-02 18:24:19 +0200

    Add TramType, fix tram <-> tramtype relationships.

--- Rails/Rails/Models/Depot.cs
+++ Rails/Rails/Models/Depot.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,9 @@ namespace Rails.Models
 {
     public class Depot
     {
-
+        [Key]
         public int Id { get; set; }
-
-
         public string Name { get; set; }
-
         public int FullMaintenanceRoutines { get; set; }
         public int QuickMaintenanceRoutines { get; set; }
         public int FullCleanRoutines { get; set; }
--- Rails/Rails/Models/Tram.cs
+++ Rails/Rails/Models/Tram.cs
@@ -11,26 +11,18 @@ namespace Rails.Models
     {
         [Key]
         public int Id { get; set; }
-
-        public int DepotId { get; set; }
-
-        public int TramTypeId { get; set; }
-
         public int Number { get; set; }
-
         public int Length { get; set; }
-
         public string Condition { get; set; }
-
         public int Dirty { get; set; }
-
-
         public int Broken { get; set; }
-
-
         public int DriverQualified { get; set; }
-
-
         public int Available { get; set; }
+
+        public virtual Depot Depot { get; set; }
+        public virtual TramType TramType { get; set; }
+
+        public int? DepotId { get; set; }
+        public int? TramTypeId { get; set; }
     }
 }
