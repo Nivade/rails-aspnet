209fa5ef750ad4197a404d414fe7b70f87bba36e
Nivade <nhavandeursen@gmail.com>
2016-06-02 22:52:27 +0200

    Add sector controller and views.

---
+++ Rails/Rails/Controllers/SectorController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rails.Models;
+
+namespace Rails.Controllers
+{
+    public class SectorController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Sector
+        public ActionResult Index()
+        {
+            var sectors = db.Sectors.Include(s => s.Track).Include(s => s.Tram);
+            return View(sectors.ToList());
+        }
+
+        // GET: Sector/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sector sector = db.Sectors.Find(id);
+            if (sector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sector);
+        }
+
+        // GET: Sector/Create
+        public ActionResult Create()
+        {
+            ViewBag.TrackId = new SelectList(db.Tracks, "Id", "Id");
+            ViewBag.TramId = new SelectList(db.Trams, "Id", "Condition");
+            return View();
+        }
+
+        // POST: Sector/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Number,Accessible,Blocked,TramId,TrackId")] Sector sector)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sectors.Add(sector);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TrackId = new SelectList(db.Tracks, "Id", "Id", sector.TrackId);
+            ViewBag.TramId = new SelectList(db.Trams, "Id", "Condition", sector.TramId);
+            return View(sector);
+        }
+
+        // GET: Sector/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sector sector = db.Sectors.Find(id);
+            if (sector == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TrackId = new SelectList(db.Tracks, "Id", "Id", sector.TrackId);
+            ViewBag.TramId = new SelectList(db.Trams, "Id", "Condition", sector.TramId);
+            return View(sector);
+        }
+
+        // POST: Sector/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Number,Accessible,Blocked,TramId,TrackId")] Sector sector)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sector).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TrackId = new SelectList(db.Tracks, "Id", "Id", sector.TrackId);
+            ViewBag.TramId = new SelectList(db.Trams, "Id", "Condition", sector.TramId);
+            return View(sector);
+        }
+
+        // GET: Sector/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sector sector = db.Sectors.Find(id);
+            if (sector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sector);
+        }
+
+        // POST: Sector/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Sector sector = db.Sectors.Find(id);
+            db.Sectors.Remove(sector);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
